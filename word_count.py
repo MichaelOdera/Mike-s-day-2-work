d4e51dc82d2d81df2488e96b6153135acdc14ac7
Michael Odera <michogelira@gmail.com>
2016-11-14 15:30:35 +0300

    Add files via upload
    This is my Word count assignment with comments made within the code

---
+++ word_count.py
@@ -0,0 +1,21 @@
+# First I create a function that takes in an attribute "string"
+def words(string):
+      # the second step requires that we count all words using str as a
+      # separator
+   	my_string = string.split()
+      #We then create an empty dictionary called "my_dict"
+      # It's purpose is to store the number of words in the string "my_string"
+   	my_dict = {}
+   	for item in my_string:
+   		# This step tests for integer values
+         try:                    
+   			item2 = int(item)
+   		except:
+   			item2 = item
+
+         #Loop through the words and add the count into "my_dict"
+   		if item2 in my_dict:   
+   			my_dict[item2] += 1
+   		else:
+   			my_dict[item2] = 1
+   	return my_dict
